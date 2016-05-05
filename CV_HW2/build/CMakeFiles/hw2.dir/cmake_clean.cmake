12ec05f99a1527c42ef3acaeef4bdc55f971bd38
s880367 <s880367@gmail.com>
2016-05-05 20:05:25 +0800

    good_to_download

---
+++ CV_HW2/build/CMakeFiles/hw2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hw2.dir/SIFT_opencv2.4x.cpp.o"
+  "hw2.pdb"
+  "hw2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
